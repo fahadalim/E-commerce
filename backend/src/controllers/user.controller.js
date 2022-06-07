6d67e285b7ed529c3fba045192515622400000d3
fahadalim <mohammadfahadalim@gmail.com>
2022-06-07 17:26:20 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,15 +1,16 @@
-const express = require("express")
-const User =  require("../models/users")
+const express = require("express");
+const User = require("../models/users");
 const router = express.Router();
 
 //data of all users
-router.get("/",async(req,res)=>{
-    try{
-        const users = await User.find().lean().exec();
-        res.status(200).send({data:users,message:"success"});
-    }
-    catch(error){
-        console.log("error:",error);
-        res.status(500).send({data:[],message:"error"})
-    }
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().lean().exec();
+    res.status(200).send({ data: users, message: "success" });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).send({ data: [], message: "error" });
+  }
 });
+
+//for creating user
