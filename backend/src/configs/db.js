49b7eda0a9b8db696a716919e2a7494626059ab9
fahadalim <mohammadfahadalim@gmail.com>
2022-06-07 12:54:49 +0530

    dbconnect

---
+++ backend/src/configs/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const connect  =()=>{
+    return mongoose.connect(
+        "mongodb+srv://ecommerce:wbOn5b3u4tfI18V9@cluster0.wa68wk9.mongodb.net/?retryWrites=true&w=majority"
+    )
+}
+
+module.exports = connect
+
+
+// wbOn5b3u4tfI18V9
