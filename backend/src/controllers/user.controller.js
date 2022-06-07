8666068a151de90a0d3eba5c3b83f83f8a8fff62
fahadalim <mohammadfahadalim@gmail.com>
2022-06-07 17:33:29 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -14,3 +14,23 @@ router.get("/", async (req, res) => {
 });
 
 //for creating user
+router.post("/create", async (req, res) => {
+  try {
+    const user = await User.create(req, body);
+    return res.status(201).send({ data: user, message: "success" });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).send({ data: [], message: "error" });
+  }
+});
+
+//for getting single user
+router.get("/:id", async (req, res) => {
+  try {
+    const users = await User.findById(req.params.id).lean().exec();
+    res.status(200).send({ data: users, message: "success" });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).send({ data: [], message: "error" });
+  }
+});
