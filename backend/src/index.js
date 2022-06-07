32c0e4a0f029704ae45d3517a174117b2fa8ebea
fahadalim <mohammadfahadalim@gmail.com>
2022-06-07 23:02:04 +0530

    user part

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const userController = require("./controllers/user.controller");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h3>welcome</h1>");
+});
+
+app.use("/users", userController);
 
 module.exports = app;
